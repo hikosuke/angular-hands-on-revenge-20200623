03a99027a647c924abb1fa7a281cee107e7130bb
hikosuke <keisuke4628@gmail.com>
2020-06-24 02:51:20 +0900

    3 files modified, 2 added

---
+++ src/app/todoList.ts
@@ -0,0 +1,16 @@
+import { Todo } from './todo';
+
+export const todoList: Todo[] = [
+  {
+    title: '会社説明会の対応をする',
+    completed: false
+  },
+  {
+    title: '並木にお礼する',
+    completed: false
+  },
+  {
+    title: 'angular hands-onの復習',
+    completed: true
+  }
+];
